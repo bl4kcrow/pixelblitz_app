c8c17e48ed96b53740cae0334bf20aac62c289a8
Azael Ortega <azortega@live.com.mx>
2023-06-15 11:30:33 -0600

    Initialize the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixelblitz_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/bl4kcrow/pixelblitz_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bl4kcrow.pixelblitz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
